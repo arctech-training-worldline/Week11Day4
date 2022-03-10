7a18e496324fe070c41dc225b8347ac6e6738a39
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-03-10 17:50:26 +0530

    Add project files.

---
+++ Week11Day4/Services/BankInfoService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using WorldlineLiveServiceReference;
+using System.Linq;
+
+namespace Week11Day4.Services
+{
+    public class BankInfoService : IBankInfoService
+    {
+        public readonly string serviceUrl = "https://bankdetailsdemo.azurewebsites.net/WebServices/SoapDemo.asmx";
+        public readonly EndpointAddress endpointAddress;
+        public readonly BasicHttpBinding basicHttpBinding;
+
+        public BankInfoService()
+        {
+            endpointAddress = new EndpointAddress(serviceUrl);
+
+            basicHttpBinding = new BasicHttpBinding(
+                endpointAddress.Uri.Scheme.ToLower() == "http" ?
+                    BasicHttpSecurityMode.None :
+                    BasicHttpSecurityMode.Transport)
+            {
+                OpenTimeout = TimeSpan.MaxValue,
+                CloseTimeout = TimeSpan.MaxValue,
+                ReceiveTimeout = TimeSpan.MaxValue,
+                SendTimeout = TimeSpan.MaxValue
+            };
+        }
+
+        private async Task<SoapDemoSoapClient> GetInstanceAsync()
+        {
+            return await Task.Run(() => new SoapDemoSoapClient(basicHttpBinding, endpointAddress));
+        }
+
+        public async Task<List<BankDetails>> GetByIfscAsync(string ifsc)
+        {
+            var wsClient = await GetInstanceAsync();
+            var getBranchDetailsByIfscResponse = await wsClient.GetBranchDetailsByIfscAsync(ifsc);
+
+            return new List<BankDetails> { getBranchDetailsByIfscResponse.Body.GetBranchDetailsByIfscResult };
+        }
+    }
+}
